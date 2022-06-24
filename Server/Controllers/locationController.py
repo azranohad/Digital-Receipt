57d2f52f9d56101adf49181a6a8b40acd84cdc1e
amiti <amitilovitch@gmail.com>
2022-06-24 10:28:41 +0300

    change server locationController.py

--- Server/Controllers/locationController.py
+++ Server/Controllers/locationController.py
@@ -19,10 +19,13 @@ def get_nearest_store():
 
     return store_location_service.find_nearest_store_to_point(user_key, str_location)
 
-@location_api.route('/get_store_credit_nearest_to_location', methods=['GET'])
-def get_store_credit_nearest_to_location():
-    str_location = request.get_json(force=True)['location']
+@location_api.route('/get_nearest_store', methods=['GET','POST'])
+def get_nearest_store():
+    #str_location = request.get_json(force=True)['location']
+    str_latitude = request.get_json(force=True)['latitude']
+    str_longitude = request.get_json(force=True)['longitude']
+    location = [float(str_latitude), float(str_longitude)]
     user_key = request.get_json(force=True)['user_key']
-    logger.print_api_message("received get_nearest_store post request | location: " + str_location)
+    logger.print_api_message("received get_nearest_store post request | location: " + str(location))
 
-    return store_location_service.find_nearest_store_to_point(user_key, str_location)
+    return store_location_service.find_nearest_store_to_point(user_key, location)
